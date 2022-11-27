91a8b7c877a8bb8d388468609ea3e5a37db65ef4
DKworgan <worganstlaurent@gmail.com>
2022-11-27 17:37:27 -0500

    solve exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius;
+
+  celsius = (temp - 32) * 5/9;
+
+  //round to nearest decimal
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit;
+
+  fahrenheit = (temp * 9/5) + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+
+  return fahrenheit;
+
 };
 
 // Do not edit below this line
