929bb9355c318fa56e66af4b20294de8eb5d57fa
DKworgan <worganstlaurent@gmail.com>
2022-11-26 22:18:33 -0500

    finish 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let stringLen = stringToReverse.length;
+    let reversedString = stringToReverse[stringLen - 1];
+    
+    if(stringToReverse == ""){
+        return "";
+    }
+    for(let i = stringToReverse.length - 2; i > -1; i--){
+        reversedString += stringToReverse[i];
+    }
 
+    console.log(reversedString);
+    return reversedString;
 };
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
