4f792c8a15b2bd18060683dac1bb9224eb938157
DKworgan <worganstlaurent@gmail.com>
2022-11-27 18:15:54 -0500

    solve exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,51 @@
-const add = function() {
+const add = function(num1, num2) {
+	return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+
+const sum = function(arr) {
+  let total = 0;
+  
+  for(let i = 0; i < arr.length; i++){
+    total += arr[i];
+  }
+
+  return total;
+};
+
+const multiply = function(arr) {
+  let total = arr[0];
+
+  for(let i = 1; i < arr.length; i++){
+    total *= arr[i];
+  }
+
+  return total;
+};
+
+const power = function(num, exp) {
+  return Math.pow(num, exp);
 	
 };
 
-const subtract = function() {
-	
+const factorial = function(num) {
+  if(num == 0){
+    return 1;
+  }
+
+  let total = 1;
+
+  for(let i = 1; i < num + 1; i++){
+    total *= i;
+  }
+  
+  return total;
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
+factorial(1);
 // Do not edit below this line
 module.exports = {
   add,
