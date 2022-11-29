a966fd0754d69ba2124a8fd04a03765c182545b4
DKworgan <worganstlaurent@gmail.com>
2022-11-28 22:20:22 -0500

    solve exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let reversedWord = " "; 
+    let parsedWord = " ";
 
+    word = word.toLowerCase();
+
+    //appends character, if character falls between 
+    // lower case a - z
+    for(let i = 0; i < word.length; i++){
+        if(word.charCodeAt(i) >= 97 && word.charCodeAt(i) <= 122){
+            parsedWord += word.charAt(i);
+            //console.log(word.charAt(i));
+        }
+    }
+     parsedWord = parsedWord.trim();
+
+
+    for(let i = parsedWord.length - 1; i > -1; i--){
+        reversedWord += parsedWord[i];
+    }
+    
+    reversedWord = reversedWord.trim();
+
+    if(reversedWord == parsedWord){
+        return true;
+    } else {
+        return false;
+    }
 };
 
+
+//palindromes("race");
 // Do not edit below this line
 module.exports = palindromes;
